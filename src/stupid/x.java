38fa0a0969e28d5ab57919b23598dc102f28e199
tuan1kdt <40836785+tuan1kdt@users.noreply.github.com>
2018-10-29 09:24:12 +0700

    Update x.java

--- src/stupid/x.java
+++ src/stupid/x.java
@@ -4,14 +4,14 @@ import java.nio.file.Files;import java.nio.file.Paths;public class x {
     public static void main(String[] arg)   {        int CC=hohoho(arg);System.exit(CC);    }
     public static int hohoho(String uuu[]) {
         try{if
-        (uuu.length != 2) return
--1
-            ;String bbb,ddd= new String(Files.readAllBytes(Paths.get(uuu[1]))); 
+        (uuu.length != 2) 
+            return -1;
+            String bbb,ddd= new String(Files.readAllBytes(Paths.get(uuu[1]))); 
             bbb= new String(Files.readAllBytes(Paths.get(uuu[0])));
             int kkk = bbb.length();
-            if(kkk<4)               return
--2
-            ;int RRR=0; float eee=0;
+            if(kkk<4)              
+                return -2;
+            int RRR=0; float eee=0;
             for(int aaa=0;aaa<kkk-3;aaa++)
             {       for(int qqq=0;qqq<ddd.length()-4;qqq+=1)             {int vOv = 0;
                     for(int CC=0;CC<4;CC++){
@@ -20,11 +20,11 @@ import java.nio.file.Files;import java.nio.file.Paths;public class x {
                 {
                     System.out.println("DEBUG:".concat(String.valueOf(aaa) + "," + String.valueOf(qqq)));
                 RRR++; eee+=1;}
-                else             ++RRR;                         }}
-            System.out.println(String.valueOf(Math.round(eee)).concat("/")
-+ String.valueOf(RRR));   return
-1
-                            ;} catch(IOException
-            ex){System.out.println("DEBUG:Error");return
--3
-                            ;}}}
+                else  ++RRR;                         }}
+            System.out.println(String.valueOf(Math.round(eee)).concat("/") + String.valueOf(RRR));   return 1;} 
+        catch(IOException
+            ex){System.out.println("DEBUG:Error");
+                return -3;
+               }
+    }
+}
